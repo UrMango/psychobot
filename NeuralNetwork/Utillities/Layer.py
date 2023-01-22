ee58cee6094a37f26bb6368747089b7a244e0db6
DolevD42 <dolevdilmoney@gmail.com>
2023-01-23 01:28:53 +0200

    Update Layer.py

--- NeuralNetwork/Utillities/Layer.py
+++ NeuralNetwork/Utillities/Layer.py
@@ -7,21 +7,19 @@ class LayerType(Enum):
     MIDDLE = 2
     MULTIPLY = 3
     SOFTMAX = 4
+    ADD = 5
 
 class Layer:
     def __init__(self):
-        self.inputs_id = None #All the id of layers that this layer get as input
-        self.outputs_id = None #All the id of the layers that get this layer as input
+        self.inputs_id = None  # All the id of layers that this layer get as input
         self.output = None
 
         self.type = LayerType.NONE
         self.id = None
 
-    def forward_propagation(self, output_layers_dict, time):
+    # def forward_propagation(self, output_layers_dict, time):
+    #     raise NotImplementedError
+
+    def nudge(self, nudge_layers_dict, learning_rate):
         raise NotImplementedError
 
-    def backward_propagation(self, output_nudge):
-        raise NotImplementedError
-
-    def update_parameters(self, dict_nudge):
-        raise NotImplementedError
