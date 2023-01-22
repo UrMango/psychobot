36de09bb2a6ef9f0a051409ce7bbcbd376559fc0
DolevD42 <dolevdilmoney@gmail.com>
2023-01-23 01:28:39 +0200

    Create AddLayer.py

---
+++ NeuralNetwork/Utillities/AddLayer.py
@@ -0,0 +1,47 @@
+from NeuralNetwork.Utillities.Layer import Layer, LayerType
+import numpy as np
+
+class AddLayer(Layer):
+	def __init__(self, _id, _inputs_id):
+		super().__init__()
+		self.input = None
+		self.inputs_id = _inputs_id
+		self.id = _id
+		self.type = LayerType.ADD
+
+	def forward_propagation(self, output_layers_dict, t):
+
+		time = str(t)
+		self.input = []
+		for input_id in self.inputs_id:
+			if input_id[-1] == "-":
+				self.input.append(output_layers_dict[input_id[:-1]+str(t-1)])
+			else:
+				self.input.append(output_layers_dict[input_id+time])
+		self.output = np.add(self.input[0], self.input[1])
+		output_layers_dict[self.id+time] = self.output
+		self.input = []
+		return output_layers_dict
+
+	def backward_propagation(self, nudge_layers_dict, output_layers_dict, t):
+		self.input = []
+		time = str(t)
+		for input_id in self.inputs_id:
+			if input_id[-1] == "-":
+				self.input.append(output_layers_dict[input_id[:-1]+str(t-1)])
+			else:
+				self.input.append(output_layers_dict[input_id+time])
+		output_nudge = nudge_layers_dict["d"+self.id+time]
+
+		for input_id in self.inputs_id:
+			if input_id[-1] == "-":
+				key = "d"+input_id[:-1]+str(t-1)
+			else:
+				key = "d"+input_id+time
+			nudge_layers_dict[key] = output_nudge
+
+		self.input = []
+		return nudge_layers_dict
+
+	def nudge(self, nudge_layers_dict, learning_rate):
+		pass
