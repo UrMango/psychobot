67cc67ec9d5fdbe7b3cdb9f50c4361991dda2431
DolevD42 <dolevdilmoney@gmail.com>
2023-01-23 01:28:44 +0200

    Update MultiplyLayer.py

--- NeuralNetwork/Utillities/MultiplyLayer.py
+++ NeuralNetwork/Utillities/MultiplyLayer.py
@@ -2,37 +2,47 @@ from NeuralNetwork.Utillities.Layer import Layer, LayerType
 import numpy as np
 
 class MultiplyLayer(Layer):
-	def __init__(self, _nudges_id, _id, _inputs_id, _outputs_id):
+	def __init__(self, _id, _inputs_id):
 		super().__init__()
 		self.input = None
 		self.inputs_id = _inputs_id
-		self.outputs_id = _outputs_id
-		self.nudges_id = _nudges_id
 		self.id = _id
 		self.type = LayerType.MULTIPLY
 
-	def forward_propagation(self, output_layers_dict, time):
+	def forward_propagation(self, output_layers_dict, t):
+
+		time = str(t)
 		self.input = []
 		for input_id in self.inputs_id:
-			self.input.append(output_layers_dict[input_id+time])
-		self.output = np.multiply(self.input[0], self.input[1])
+			if input_id[-1] == "-":
+				self.input.append(output_layers_dict[input_id[:-1]+str(t-1)])
+			else:
+				self.input.append(output_layers_dict[input_id+time])
+		self.output = np.multiply(self.input[0], self.input[1])  # assume there is only 2 vectors
 		output_layers_dict[self.id+time] = self.output
 		self.input = []
 		return output_layers_dict
 
-	def backward_propagation(self, nudge_layers_dict, output_layers_dict, time):
+
+	def backward_propagation(self, nudge_layers_dict, output_layers_dict, t):
 		self.input = []
+		time = str(t)
 		for input_id in self.inputs_id:
-			self.input.append(output_layers_dict[input_id+time])
-		output_nudge = np.zeros((1, self.output_size), dtype=np.float32)
-		for output_id in self.outputs_id:
-			output_nudge = np.add(nudge_layers_dict[output_id+time], output_nudge) #sum up all the nudges
+			if input_id[-1] == "-":
+				self.input.append(output_layers_dict[input_id[:-1]+str(t-1)])
+			else:
+				self.input.append(output_layers_dict[input_id+time])
+		output_nudge = nudge_layers_dict["d"+self.id+time]
 		nudges = []
-		nudges.append(np.multiply(output_nudge, self.input[1])) # the nudge of the *first* input
-		nudges.append(np.multiply(output_nudge, self.input[0])) # the nudge of the *second* input
+		nudges.append(np.multiply(output_nudge, self.input[1]))  # the nudge of the *first* input
+		nudges.append(np.multiply(output_nudge, self.input[0]))  # the nudge of the *second* input
 		i = 0
-		for id in self.nudges_id:
-			nudge_layers_dict[id+time] = nudges[i]
+		for input_id in self.inputs_id:
+			if input_id[-1] == "-":
+				key = "d"+input_id[:-1]+str(t-1)
+			else:
+				key = "d"+input_id+time
+			nudge_layers_dict[key] = nudges[i]
 			i += 1
 
 		self.input = []
