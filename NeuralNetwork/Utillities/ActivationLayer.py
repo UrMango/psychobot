ae5188dddd3bf4b4d205de74ae672aa461b3ec32
DolevD42 <dolevdilmoney@gmail.com>
2023-01-22 12:15:01 +0200

    Update ActivationLayer.py
    make the Activation layer fitable for RNN

--- NeuralNetwork/Utillities/ActivationLayer.py
+++ NeuralNetwork/Utillities/ActivationLayer.py
@@ -2,20 +2,33 @@ from NeuralNetwork.Utillities.Layer import Layer, LayerType
 import numpy as np
 
 class ActivationLayer(Layer):
-	def __init__(self, function, derivative, id):
+	def __init__(self, _function, _derivative, _id, _inputs_id, _outputs_id):
 		super().__init__()
-		self.function = function
-		self.derivative = derivative
-		self.id = id
+		self.input = None
+		self.inputs_id = _inputs_id
+		self.outputs_id = _outputs_id
+		self.function = _function
+		self.derivative = _derivative
+		self.id = _id
 		self.type = LayerType.ACTIVATION
 
-	def forward_propagation(self, list_of_inputs,dict):
-		self.input = list_of_inputs[0]
-		self.output = self.function( list_of_inputs[0])
-		return self.output
+	def forward_propagation(self, output_layers_dict, time):
+		for input_id in self.inputs_id:
+			self.input = output_layers_dict[input_id+time]
+		self.output = self.function(self.input) #making the activation
+		output_layers_dict[self.id+time] = self.output
+		self.input = None
+		return output_layers_dict
 
-	def backward_propagation(self, output_nudge,dict):
-		return self.derivative(self.input)*output_nudge
+	def backward_propagation(self, nudge_layers_dict, output_layers_dict, time):
+		for input_id in self.inputs_id:
+			self.input = output_layers_dict[input_id+time]
+		output_nudge = np.zeros((1, self.output_size), dtype=np.float32)
+		for output_id in self.outputs_id:
+			output_nudge = np.add(nudge_layers_dict[output_id+time], output_nudge) #sum up all the nudges
+		nudge_layers_dict["d" + self.id+time] = self.derivative(self.output)*output_nudge #defind the gradiant of the layer and put it in the
+		self.input = None
+		return nudge_layers_dict
 
-	def nudge(self, dict):
+	def nudge(self, nudge_layers_dict, learning_rate):
 		pass
